9ce16143bb98b43fa3ce3814c321b5d3b4a04762
ShyamSrinivasan <shyamseenufirst@gmail.com>
2025-03-23 15:16:17 +0530

    .

--- Exercise 4/Fragment/app/src/main/java/com/example/ex4_fragment/MainActivity.kt
+++ Exercise 4/Fragment/app/src/main/java/com/example/ex4_fragment/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.ex4_fragment.ui.theme.Ex4_FragmentTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Ex4_FragmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Ex4_FragmentTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
